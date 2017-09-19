575325c69d8742c5dca613b364a40499a0152eec
Doug Lawrence <Doug.Lawrence@dnvgl.com>
2017-09-19 10:41:07 +0100

    Added first control

---
+++ DemoSolution/HomeControl/HomeCtrl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HomeControl
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class HomeCtrl : UserControl
+    {
+        public HomeCtrl()
+        {
+            InitializeComponent();
+        }
+    }
+}
